78ebd0bd5f90fc5149c8373bffef22529b678f1d
Michał Jewczuk <michal@jewczuk.net>
2018-05-10 09:58:38 +0200

    Dodanie helpers manipulacja id do pobierania id z nazw podmiotów w listach

---
+++ app/helpers/manipulacja_id.php
@@ -0,0 +1,21 @@
+<?php
+  
+  function pobierzIdNazwy($nazwa) {
+    /*
+     * funkcja która pobiera numer (id) z podanego ciągu w formacie:
+     * id# pozostały tekst 
+     * zwraca id jako string
+     */
+
+    $el = explode('#', $nazwa);
+    return $el[0];
+  }
+
+  function utworzIdNazwa($id, $nazwa) {
+    /*
+     * funkcja która tworzy ciąg typu id# nazwa z podanych danych
+     * zwraca string
+     */
+
+    return $id . '# ' . $nazwa; 
+  }
